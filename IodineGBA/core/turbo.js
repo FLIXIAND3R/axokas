d4b681ff2dadb997e61ec6efbd84e0266eecb2f0
FLIXIAND3R <felix@felixgeens.com>
2025-12-05 14:10:54 +0000

    Create turbo.js

---
+++ IodineGBA/core/turbo.js
@@ -0,0 +1,15 @@
+let speedMultiplier = 1;
+const TURBO_FACTOR = 4;
+
+const turboBtn = document.getElementById('turbo-btn');
+if (turboBtn) {
+  turboBtn.addEventListener('click', () => {
+    if (speedMultiplier === 1) {
+      speedMultiplier = TURBO_FACTOR;
+      turboBtn.innerText = 'Turbo: ON';
+    } else {
+      speedMultiplier = 1;
+      turboBtn.innerText = 'Turbo: OFF';
+    }
+  });
+}
